a6c2967b6e053138376905787255ea2beafcc83e
Geziel Elyon <geziel_barbosa_@hotmail.com>
2025-11-21 18:43:47 -0300

    style: add default background color at body tag style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{ backgroundColor: "#000" }}>{children}</body>
     </html>
   );
 }
